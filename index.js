c0657036047142d1af5ac380464d63d3e8285614
JordiJacas <jjacasventura@iesesteveterradas.cat>
2019-04-01 19:46:09 +0200

    cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 
 var express = require('express');
 var bodyParser = require('body-parser')
+var cors = require('cors')
 var app = express()
+
+app.use(cors());
 
 // create application/json parser
 var jsonParser = bodyParser.json()
