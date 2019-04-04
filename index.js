a6eac98af2ba0847f1b1e56dd89367abb7f25aeb
JordiJacas <jjacasventura@iesesteveterradas.cat>
2019-04-04 19:58:19 +0200

    database

--- index.js
+++ index.js
@@ -66,8 +66,8 @@ app.get('/api/login/:username/:password', function (req, res){
 app.get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query("SELECT * FROM test_table WHERE name='Jordi' AND password='123'");
-      //const results = { 'results': (result) ? result.rows : null};
+      const result = await client.query("SELECT * FROM test_table WHERE name='Jo' AND password='123'");
+      const results = { 'results': (result) ? result.rows : null};
       //res.render('pages', results );
       res.send(JSON.stringify(result));
       client.release();
